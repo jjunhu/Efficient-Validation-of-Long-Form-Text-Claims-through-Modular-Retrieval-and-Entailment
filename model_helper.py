f024faf525333c2a91c0b0011fc446e36eddee31
jjunhu <jonathanjun.hu@gmail.com>
2024-03-12 17:07:31 -0400

    more retrieval results with different levels of summary data

--- model_helper.py
+++ model_helper.py
@@ -7,6 +7,16 @@ QUERY = "depth_3_summary"
 DOCUMENT = "book_text"
 FILE_PATH = 'data/extracted_data.csv'
 
+
+l2_QUERY = "depth_2_summary"
+l2_DOCUMENT = "book_text"
+l2_FILE_PATH = 'data/l2_extracted_data.csv'
+
+
+l1_QUERY = "depth_1_summary"
+l1_DOCUMENT = "book_text"
+l1_FILE_PATH = 'data/l1_extracted_data.csv'
+
 # get the first 25 books for the 175B
 def get_evaluation_set_175():
     data = pd.read_csv(FILE_PATH)
@@ -18,6 +28,32 @@ def get_evaluation_set_6():
 
 def get_evaluation_set():
     data = pd.read_csv(FILE_PATH)
+    return data[:4000]
+
+# TODO: change data size for evaluation
+
+def l2_get_evaluation_set_175():
+    data = pd.read_csv(l2_FILE_PATH)
+    return data[(data["book_num"] < 25) & (data["model_size"] == "175b")]
+
+def l2_get_evaluation_set_6():
+    data = pd.read_csv(l2_FILE_PATH)
+    return data[(data["book_num"] < 25) & (data["model_size"] == "6b")]
+
+def l2_get_evaluation_set():
+    data = pd.read_csv(l2_FILE_PATH)
+    return data[:4000]
+
+def l1_get_evaluation_set_175():
+    data = pd.read_csv(l1_FILE_PATH)
+    return data[(data["book_num"] < 25) & (data["model_size"] == "175b")]
+
+def l1_get_evaluation_set_6():
+    data = pd.read_csv(l1_FILE_PATH)
+    return data[(data["book_num"] < 25) & (data["model_size"] == "6b")]
+
+def l1_get_evaluation_set():
+    data = pd.read_csv(l1_FILE_PATH)
     return data[:4000]
 
 def two_gram_overlap(str1, str2):
